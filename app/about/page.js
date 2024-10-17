2e9bc2b9afd764077537e4717274dc0fc62ef9a1
anupBurman <anupburman24@gmail.com>
2024-10-17 13:15:57 +0530

    API data
    API data from external server

--- app/about/page.js
+++ app/about/page.js
@@ -1,27 +1,51 @@
-
+'use client';
 import Image from "next/image";
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function About() {
 
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function getUsers() {
+      const usersData = await fetch('https://jsonplaceholder.typicode.com/users');
+      setUsers(await usersData.json())
+    }
+    getUsers();
+  }, [])
+  // console.log(users);
   return (
 
     <>
       <div className="container">
-        <h2> Main container </h2>
+        <h2> About Us</h2>
         <Button variant="primary">Primary</Button>{' '}
         <Button variant="secondary">Secondary</Button>{' '}
-        <div className="row">
-          <div className="col-6">
-            <p> First Block start from here </p>
+        <div className="row mt-4">
+          <h4> Our Dedicated Employee's </h4>
+          <div className="col-3">
+            <ol>
+              {
+                users.map((user) =>
+                  <li key={user.id}> {user.name} </li>
+                )
+              }
+
+            </ol>
           </div>
-          <div className="col-6">
-            <p> First Block start from here </p>
+          <div className="col-9">
+            <ul>
+              {
+                users.map((user) =>
+                  <li key={user.id}> {user.email} </li>
+                )
+              }
+            </ul>
           </div>
-        </div>        
+        </div>
 
       </div>
 
