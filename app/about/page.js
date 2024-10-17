cd2d731d1488862fb4718b4e7e6eb78c89bc7f82
anupBurman <anupburman24@gmail.com>
2024-10-17 18:02:49 +0530

    few new changes

--- app/about/page.js
+++ app/about/page.js
@@ -4,35 +4,46 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import styles from "./page.module.css";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function About() {
 
-  // const [users, setUsers] = useState([]);
-  // useEffect(() => {
-  //   async function getUsers() {
-  //     const usersData = await fetch('https://jsonplaceholder.typicode.com/users');
-  //     setUsers(await usersData.json())
-  //   }
-  //   getUsers();
-  // }, [])
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function getUsers() {
+      const usersData = await fetch('https://jsonplaceholder.typicode.com/users');
+      setUsers(await usersData.json())
+    }
+    getUsers();
+  }, [])
   return (
 
     <>
       <div className="container">
-        <h2> About Us</h2>
+        <h2> About Us </h2>
         <Button variant="primary">Primary</Button>{' '}
         <Button variant="secondary">Secondary</Button>{' '}
         <div className="row mt-4">
           <h4> Our Dedicated Employee's </h4>
           <div className="col-3">
             <ol>
-              <li> <b>Employee Name </b> </li>
+              <li> <b>Employee Name's </b> </li>
+              {
+                users.map((user) =>
+                  <li key={user.id}> {user.name} </li>
+                )
+              }
+
             </ol>
           </div>
           <div className="col-9">
             <ul>
             <li> <b>Employee Email </b> </li>
+              {
+                users.map((user) =>
+                  <li key={user.id}> {user.email} </li>
+                )
+              }
             </ul>
           </div>
         </div>
